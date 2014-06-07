4394c0a44e402dd1c51e9235ed7393a461f15b9d
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-07 03:44:10 -0400

    render update

--- datatable/js/script.js
+++ datatable/js/script.js
@@ -1,9 +1,29 @@
 $(document).ready( function () {
-	$('#brs').on( 'click', function() {
-		var table = $('#elemTable').DataTable({destroy:true, data: _.reject(data, function(row){ return row.m5 == null} )});
-		table.search('beach').draw();
-	  });
+	var jsondata;
+	var coldef1= [
+		        { data: 'name'},
+		     	{ data: 's5', render: scoreRender },
+		        { data: 'm3', render: scoreRender },
+	        	{ data: 'm4', render: scoreRender },
+		        { data: 'm5', render: scoreRender },
+		        { data: 'r3', render: scoreRender },
+		        { data: 'r4', render: scoreRender },
+		        { data: 'r5', render: scoreRender },
+		        { data: 'w4', render: scoreRender }
+		    ];
+
+
+$.getJSON("./js/data/elementary.json", function(data) {
+jsondata = data;
+createTable($('#elemTable'), data, coldef1);
 });
+
+$('#brs').on( 'click', function() {
+	createTable(
+		$('#elemTable'),
+		_.reject(jsondata, function(row){ return row.m5 == null}),
+		 coldef1);
+  });
 
 function scoreRender(data, type) {
 	if(type === 'display')
@@ -13,3 +33,15 @@ function scoreRender(data, type) {
 	}
  	return data;
 }
+
+function createTable(table, data, coldef) {
+	table.DataTable(
+	{
+		scrollY: 560,
+		paging: true,
+		data: data, //_.reject(data, function(row){ return row.m5 == null} ),
+	    columns: coldef1,
+	    destroy:true
+	 });
+}
+});
