8da79e43c7f7f381b2844035fbe9555a585ff28b
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-10 11:06:59 -0400

    broken header

--- www/js/script.js
+++ www/js/script.js
@@ -33,18 +33,15 @@ $(document).ready( function () {
 	//Load the school lists and create table from it
 	//TODO: add error checks.
 	$.getJSON("./js/data/elementary.json", function(data) {	
-		nukeAndCreateTable($('#elemTable'), data, elemCol);
-		setAvgs($('#elemTable'),elemCols)
+		nukeAndCreateTable($('#elemTable'), data, elemCol);		
 	});
 
 	$.getJSON("./js/data/middle.json", function(data) {
-		nukeAndCreateTable($('#midTable'), data, middleColdef);
-		setAvgs($('#midTable'),midCols)
+		nukeAndCreateTable($('#midTable'), data, middleColdef);		
 	});
 
 	$.getJSON("./js/data/high.json", function(data) {
 		nukeAndCreateTable($('#highTable'), data, highColdef);
-		setAvgs($('#highTable'),highCols)
 
 	// this shouldnt be here ideally. putting it here to make sure it runs after the last table is rendered
 	$('.helptxt label').append('  or click on the column names to sort.');
@@ -80,111 +77,3 @@ function nukeAndCreateTable(table, data, coldef) {
 
 	pymChild.sendHeightToParent();
 }
-function updateColors(table, coldef){
-	selector = table.selector;
-	$(table.selector+' tbody tr').each(function(index) {
-
-		for (i = 1; i < coldef.length; i++) {
-			$td=$(this).find("td").eq(i);
-    		var score = $td.html(); 
-    		
-    		if(score == "*" | score =="-")
-    			continue;
-
-    		var avg = avgs[coldef[i].data];
-    		var diff = score-avg;
-
-    		if(diff<0)
-    		{
-    			$td.css({color:'red'})
-    		}
-    		if(diff>0)
-    		{
-    			$td.css({color:'green'})
-    		}
-    		if(diff==0)
-    		{
-    			$td.css({color:'yellow'})
-    		}
-    	}
-    });	
-}
-
-function setAvgs(table, coldef)
-{
-	for(index =0; index < coldef.length; index++)
-	{
-		var currCell =table.DataTable().column(index+1).nodes().to$();
-		var subject= coldef[index];
-		var avg = avgs[subject];
-		table.DataTable().column(index+1).nodes().to$().attr('avg',avgs[subject]);		
-		//table.DataTable().column(index+1).nodes().to$().attr('avg',avgs[data]);		
-	}
-	colorOnScale(table.DataTable());
-}
-
-function colorOnScale(table){
-	
-	table.cells().eq(0).each( function ( cellIdx, i ) {
-    var cell = table.cell( cellIdx );
- 	//var cells = table.DataTable().cells().nodes();
-	/*for (var cell in cells)
-	{*/
-
-		var val = cell.data();
-		/*if(cellIdx.column == 0)
-		{
-			return;
-		}*/
-		if(isNaN(val))
-		{
-			$(cell.node()).css('background-color', 'whitesmoke');
-			return;
-		}
-
-		var avg = $(cell.node()).attr('avg');
-		
-		var diff = val-avg;
-		if((val==null)||(val==-1))
-		{
-			return;
-		}
-		var r =255,g=255,b=0;
-
-		//scale red from 255 to 0 for vals from avg to 100
-		if(diff>0)
-		{
-			r = Math.floor(255-(255*diff)/(100-avg));
-		}
-		if(diff<0)
-		{
-			g = Math.floor(255 *(val/avg));
-		}
-		var color = 'rgba('+r+','+g+',0,.6)';
-		$(cell.node()).css('background-color', color);
-	});
-}
-
-var avgs ={"m3":58,
-			"m4":63,
-			"m5":56,
-			"m6":53,
-			"m7":56,
-			"m8":47,
-			"r3":57,
-			"r4":61,
-			"r5":61,
-			"r6":60,
-			"r7":57,
-			"r8":57,
-			"r9":53,
-			"r10":55,
-			"s5":54,
-			"s8":49,
-			"w4":53,
-			"w8":56,
-			"w10":64
-		};
-	var elemCols =["s5","m3","m4","m5","r3","r4","r5","w4"];
-	var midCols = ["s8","m6","m7","m8","r6","r7","r8","w8"];
-	var highCols = ["r9","r10","w10"];
